5534689aa12a2ae6480ee95a74c59f671269938d
JensOAndersen <jensoleand@gmail.com>
2019-05-21 15:44:16 +0200

    initial commit

---
+++ Monarchs.App/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Monarchs.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+/*
+    
+*/
